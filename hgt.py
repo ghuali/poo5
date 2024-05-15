bdd6b0e4812de587a41723bd41006ce09d9639b1
ghuali <guaghuali@gmail.com>
2024-05-15 09:20:22 +0100

    Creamos clases

---
+++ hgt.py
@@ -0,0 +1,32 @@
+class Tarea:
+    pass
+
+
+
+
+
+
+
+class Evento(Tarea):
+    pass
+
+
+
+
+class ListaTareas:
+    pass
+
+
+
+
+
+class ListaEventos:
+    pass
+
+
+
+
+
+
+class Interfaz:
+    pass
