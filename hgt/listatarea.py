cd2018e41ab6cc36bfcdc580950aec10a497fd69
ghuali <guaghuali@gmail.com>
2024-05-21 12:47:27 +0100

    terminado lista tarea

--- hgt/listatarea.py
+++ hgt/listatarea.py
@@ -1,16 +1,70 @@
+from hgt.tarea import Tarea
+
 class ListaTareas:
-    
+    LIMITCHAR = "|&&|"
     def __init__(self) -> None:
+        self.tareaitas = []
+
+    def agregar(self, tarea:Tarea):
+        self.tareaitas.append(tarea)
         
 # Guardar una los valores de tarea en una lista y utilizar "CRUD" que es read  y delete
 
 
 
     def read(self):
-        pass
+        result = ""
+        for tarea in self.tareaitas:
+            result += tarea
+            if tarea != self.tareaitas[-1]:
+                result += self.LIMITCHAR
+
+        return result
+
+    def load(self, data:str):
+        tareas = data.split(self.LIMITCHAR)
+        for tarea in tareas:
+            self.tareaitas.append(tarea)
+
+    def update(self, latarea:Tarea, estado:bool,id:int,tarea:str):
+        for x in self.tareaitas:
+            if x == latarea:
+                x.update(estado,id,tarea)
+                break
 
 
+    def delete(self,tarea:Tarea):
+        for x in self.tareaitas:
+            if x == tarea:
+                x.delete()
+                break
 
+    def __str__(self):
+        return self.read()
+    
+    def __len__(self):
+        return  self.tareaitas.__len__()
+    
+    def __getitem__(self, index):
+        return self.tareaitas[index]
+    
+    def __setitem__(self, index, value):
+        self.tareaitas[index] = value
 
-    def delete(self):
-        pass
+    def __delitem__(self, index):
+        del self.tareaitas[index]
+
+    def __iter__(self):
+        return self.tareaitas.__iter__()
+    
+    def __next__(self):
+        return self.tareaitas.__next__()
+    
+    def __contains__(self, item):
+        return item in self.tareaitas
+    
+    def __eq__(self, other):
+        return self.tareaitas == other.tareaitas
+    
+    def __ne__(self, other):
+        return self.tareaitas != other.tareitas        
---
+++ tests_hgt/test_listatarea.py
@@ -0,0 +1,45 @@
+from hgt.listatarea import ListaTareas
+import pytest
+
+@pytest.fixture
+def lista():
+    return ListaTareas()
+
+# Test de la clase ListaAlumnos
+def test_listaTareas0(lista):
+    assert len(lista) == 0
+
+def test_listaTareas1(lista):
+    lista.agregar('False,1,"Dibujar un pato"')
+    assert len(lista) == 1
+
+def test_listaAlumno2(lista):
+    lista.agregar('False,1,"Dibujar un pato"')
+    lista.agregar('True,2,"Matar a Dios"')
+
+    assert len(lista) == 2
+
+    assert lista[0] == ('False,1,"Dibujar un pato"')
+    assert lista[1] == ('True,2,"Matar a Dios"')
+
+def test_listaAlumnoDelete(lista):
+    lista.agregar('True,3,"Jugar a fortnite"')
+    assert lista[0] == 'True,3,"Jugar a fortnite"'
+
+    del lista[0]
+    assert len(lista) == 0
+
+    assert 'True,3,"Jugar a fortnite"' not in lista
+
+def test_listaRead(lista):
+    lista.agregar('False,1,"Dibujar un pato"')
+    lista.agregar('True,2,"Matar a Dios"')
+
+    assert lista.read() == 'False,1,"Dibujar un pato"' + lista.LIMITCHAR + 'True,2,"Matar a Dios"'
+
+def test_listaLoad(lista):
+    lista.load('False,1,"Dibujar un pato"' + lista.LIMITCHAR + 'True,2,"Matar a Dios"')
+    assert len(lista) == 2
+
+    assert lista[0] == 'False,1,"Dibujar un pato"'
+    assert lista[1] == 'True,2,"Matar a Dios"'
