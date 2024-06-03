e58ec414e85d69e3890cbe9a858daade911e634a
ghuali <guaghuali@gmail.com>
2024-06-03 12:52:41 +0100

    hecho ejemplo base de datos

--- sqlite1/main.py
+++ sqlite1/main.py
@@ -1,13 +1,28 @@
 import sqlite3
 
 SQLMDLCREATE = '''
-    CREATE TABLE IF NOT EXITS citas (
-    id  INTEGRER PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS citas (
+    id  INTEGER PRIMARY KEY AUTOINCREMENT,
     cita TEXT NOT NULL
     )
 '''
 
-con = sqlite3.connect('citas.db')
+#con = sqlite3.connect('citas.db', isolation_level= None)
+con = sqlite3.connect('citas.db', autocommit=True)
 
+con.execute(SQLMDLCREATE)
 
+SQLDDLINSERT = '''
+    INSERT INTO citas(cita) VALUES ('La vida es  bella')
+'''
+
+con.execute(SQLDDLINSERT)
+#con.commit()
+
+SQLDDLSELECT= '''
+    SELECT * FROM citas
+'''
+
+res = con.execute(SQLDDLSELECT)
+print(res.fetchall())
 con.close()
